5e5dd7b6e26e3c6d0a45f6ed67cd888d5c429136
Pattanayak <Chandan.Pattanayak@emc.com>
2019-09-10 07:50:53 +0530

    use protoc from npm

--- build.sh
+++ build.sh
@@ -3,18 +3,21 @@
  echo "##################################"
  echo "generating proto file for go"
  echo "###################################"
- 
- protoc proto/greetpb/greet.proto --go_out=plugins=grpc:.
+ glide install
+ cd ui-client
+ npm install
+ cd ..
+ ./ui-client/node_modules/protoc/protoc/bin/protoc proto/greetpb/greet.proto --go_out=plugins=grpc:.
  echo "##########################"
  echo "building go project"
  echo "##########################"
- glide install
+ 
  go build
  echo "############################################"
  echo "building client project and generating proto"
  echo "############################################"
  cd ui-client
- npm install
+
 ./node_modules/protoc/protoc/bin/protoc --plugin=protoc-gen-ts=${project_path}/ui-client/node_modules/.bin/protoc-gen-ts.cmd --js_out=import_style=commonjs,binary:src/app/proto --ts_out=service=true:src/app/proto -I ${project_path}/proto ${project_path}/proto/**/*.proto
 
  cd ..
